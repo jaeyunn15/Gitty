a0bd87daf33d633c7bd3489f4e734bcc63227686
jaeyunn <wodbs135@gmail.com>
2022-10-28 18:18:53 +0900

    initial setting

---
+++ app/src/main/java/com/github/gitty/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.github.gitty.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color.DarkGray
+val Purple500 = Color.White
+val Purple700 = Color.White
+val Teal200 = Color.White
